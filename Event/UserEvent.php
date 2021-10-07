9d9dd4af3352d4c99020a9c5b619aac41a8ac2b4
IndyDevGuy <illusiongrouppcs@gmail.com>
2021-10-07 10:19:44 -0400

    -updated UserEvent.php

--- Event/UserEvent.php
+++ Event/UserEvent.php
@@ -12,8 +12,9 @@
 namespace FOS\UserBundle\Event;
 
 use FOS\UserBundle\Model\UserInterface;
-use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\EventDispatcher\Event;
 use Symfony\Contracts\EventDispatcher\Event;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserEvent extends Event
 {
